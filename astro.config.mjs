e3901aff6ce202c7a3c877f02207f682141c9e30
Draven4866 <252122135+Draven4866@users.noreply.github.com>
2025-12-31 06:52:50 +0100

    Update astro.config.mjs

--- astro.config.mjs
+++ astro.config.mjs
@@ -2,4 +2,8 @@
 import { defineConfig } from 'astro/config';
 
 // https://astro.build/config
-export default defineConfig({});
+export default defineConfig({
+  site: "https://draven4866.github.io",
+  base: "/campo_del_cuore",
+});
+
