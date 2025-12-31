c67a91a4d8c78f3cf8561f12bd4065aaf586ddd9
Draven4866 <252122135+Draven4866@users.noreply.github.com>
2025-12-31 09:06:05 +0100

    Update Astro config for GitHub Pages

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,9 +1,8 @@
 // @ts-check
 import { defineConfig } from 'astro/config';
 
-// https://astro.build/config
 export default defineConfig({
   site: "https://draven4866.github.io",
   base: "/campo_del_cuore",
+  trailingSlash: "always",
 });
-
